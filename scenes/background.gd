5d5acc9cc4e73ea09b539df201258e54974f7b81
Henrique Garcia Gomes do Ó <87249985+henriqueggdo@users.noreply.github.com>
2025-08-29 13:46:43 -0300

    TIleset aplicado no bg
    Background agora funcional.

--- scenes/background.gd
+++ scenes/background.gd
@@ -1,6 +1,6 @@
 extends ParallaxBackground
 
-@export var scroll_speed: Vector2 = Vector2(0, 50)
+@export var scroll_speed: Vector2 = Vector2(0, 1000)
 
 func _process(delta):
 	scroll_offset += scroll_speed * delta
